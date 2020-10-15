49046afcf1ac4bbe21eb26977882aafe437d5e30
crash0283 <Rasch83@gmail.com>
2020-10-15 00:03:58 -0500

    remove justifyContent add alignItems

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,8 @@ const IndexPage = () => {
       {
         data.allContentfulBook.edges.map(({node}) => {
           return (
-            <div style={{display: "flex", flexFlow: "column wrap", justifyContent: "center"}}>
-              <img style={{display: "block"}} src={node.coverImage.fixed.src} alt="pics"/>
+            <div style={{display: "flex", flexFlow: "column wrap", alignItems: "center"}}>
+              <img src={node.coverImage.fixed.src} alt="pics"/>
               <h2>{node.title}</h2>
               <h3>Written By {node.author}</h3>
               <h4>Published {node.yearPublished}</h4>
